02e7ae303c6e9ac65517e6c7bd4196a40f30d419
s23730 <s23730@pjwstk.edu.pl>
2022-07-06 19:00:26 +0200

    Add PurchaseHistory Model

---
+++ Models/PurchaseHistory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bid.it.Models
+{
+	public class PurchaseHistory
+	{
+        [Key]
+        public int IdPurchaseHistory { get; set; }
+        [Required]
+        public string IdUser { get; set; }
+        [ForeignKey("IdUser")]
+        public ApplicationUser ApplicationUser { get; set; }
+        [Required]
+        public int IdAuction { get; set; }
+        [ForeignKey("IdAuction")]
+        public Auction Auction { get; set; }
+        [Required]
+        public DateTime PurchaseDate { get; set; }
+        [Required]
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+    }
+}
+
